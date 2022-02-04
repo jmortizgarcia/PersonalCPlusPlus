0e70694503a182c1d45d1c3e7a3d0ba85c86baf2
jmortizgarcia <jmortizgarcia@gmail.com>
2022-02-04 18:46:06 +0100

    Pointer and References Examples

--- Personal_CPP/src/Main.cpp
+++ Personal_CPP/src/Main.cpp
@@ -1,8 +1,42 @@
 #include <iostream>
 
+#define LOG(x) std::cout << x << std::endl
+
+#pragma region POINTER_REFERENCES_EXAMPLE
+
+int Pointer_References_Example()
+{
+	//Pointer Example
+	int var = 8;
+	//Access memory adress
+	int* ptr = &var;
+	//Access data of the pointer and modify
+	*ptr = 10;
+	//var = 10 now;
+
+	//Ref Example
+	int a = 5;
+	int b = 8;
+
+	//int& ref = a;
+	//ref = b;
+	//int* ref = &a;
+	//*ref = 2;
+	//ref = &b;
+	//*ref = 1;
+
+	LOG(a);
+	LOG(b);
+
+	std::cin.get();
+	return 0;
+}
+
+#pragma endregion
+
 int main()
 {
-	std::cout << "Hello World!" << std::endl;
-
+	
+	std::cin.get();
 	return 0;
 }
